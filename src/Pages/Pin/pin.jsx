34ffd68e7d337c2fc462b5eaab99efa4ef3911e7
Vishaish Pandita <visheshpandita@Vishaishs-MacBook-Air.local>
2022-05-11 10:30:16 +0530

    Show hide icon added

--- src/Pages/Pin/pin.jsx
+++ src/Pages/Pin/pin.jsx
@@ -1,9 +1,20 @@
-import { Box, Button, TextField } from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import {
+  Box,
+  Button,
+  fabClasses,
+  IconButton,
+  Input,
+  InputAdornment,
+  TextField,
+} from "@mui/material";
 import { useState } from "react";
 import Keypad from "../../Components/Keypad";
 
 export default function Pin() {
   const [value, setValue] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
     <Box sx={{ width: "100%" }} justifyContent="center">
       <Box m={1} display="flex" justifyContent="center" textAlign="center">
@@ -15,20 +26,31 @@ export default function Pin() {
           SSN or ITIN with the one we have on file.
         </p>
       </Box>
-      <TextField
-        inputMode="numeric"
-        pattern={"[0-9]*"}
-        name={"amount"}
-        placeholder="XXX-XX-XXXX"
-        sx={{ input: { textAlign: "center" } }}
-        // className={classes.root}
-        fullWidth
-        value={value}
-        // disabled={disabled}
-        // onChange={(e) => {
-        //   doStuff();
-        // }}
-      />
+
+      <Box sx={{ position: "relative" }}>
+        <TextField
+          type={showPassword ? "text" : "password"}
+          inputMode="numeric"
+          pattern={"[0-9]*"}
+          name={"amount"}
+          placeholder="XXX-XX-XXXX"
+          sx={{ input: { textAlign: "center" } }}
+          // className={classes.root}
+          fullWidth
+          value={value}
+        />
+        <Box sx={{ position: "absolute", right: "2%", top: "15%" }}>
+          <IconButton
+            aria-label="toggle password visibility"
+            onClick={() => {
+              setShowPassword(!showPassword);
+            }}
+            // onMouseDown={handleMouseDownPassword}
+          >
+            {showPassword ? <VisibilityOff /> : <Visibility />}
+          </IconButton>
+        </Box>
+      </Box>
       <Box m={1} display="flex" justifyContent="center" textAlign="center">
         <Keypad setValue={setValue} value={value} />
       </Box>
